91e5e5ffe965ca9b036ec53987d7f4195618a891
mikici100 <remi.marechal-deville@outlook.com>
2023-04-03 23:24:34 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const templates = {
+    template1: `Subject: Template 1
+
+Dear [Recipient],
+
+This is an example of template 1.
+
+Best regards,
+
+[Your Name]
+[Your Title]
+[Your Company Name]
+[Your Contact Information]`,
+    template2: `Subject: Template 2
+
+Hello [Recipient],
+
+This is an example of template 2.
+
+Kind regards,
+
+[Your Name]
+[Your Title]
+[Your Company Name]
+[Your Contact Information]`,
+    template3: `Subject: Template 3
+
+Hi [Recipient],
+
+This is an example of template 3.
+
+Sincerely,
+
+[Your Name]
+[Your Title]
+[Your Company Name]
+[Your Contact Information]`
+};
+
+document.getElementById('templates').addEventListener('change', function () {
+    document.getElementById('template-content').textContent = templates[this.value];
+});
+
+document.getElementById('template-content').textContent = templates['template1'];
