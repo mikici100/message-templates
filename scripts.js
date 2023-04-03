8d2774f4695d0c2bdddae0d53af609ab9946a3e8
mikici100 <remi.marechal-deville@outlook.com>
2023-04-04 00:44:00 +0200

    Initial commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
-// ...
+const firebaseConfig = {
+    apiKey: "AIzaSyCubfHd72z0Yvp72rFNxKm98VeUXzEloXo",
+    authDomain: "ella-b4af0.firebaseapp.com",
+    databaseURL: "https://ella-b4af0-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ella-b4af0",
+    storageBucket: "ella-b4af0.appspot.com",
+    messagingSenderId: "143960882717",
+    appId: "1:143960882717:web:d27714e507661b727c50f4",
+    measurementId: "G-LV3FT5E9T4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+const database = firebase.database();
+
+const defaultTemplates = {
+    template1: 'Template 1 content',
+    template2: 'Template 2 content',
+    template3: 'Template 3 content'
+};
 
 function loadTemplates(callback) {
     database.ref('messageTemplates').once('value', (snapshot) => {
@@ -11,7 +32,6 @@ function saveTemplates(templates, names) {
     database.ref('messageTemplates').set({ templates, names });
 }
 
-// Load templates from the database
 loadTemplates((templates, names) => {
     function updateTemplateContent() {
         document.getElementById('template-content').value = templates[this.value];
@@ -19,7 +39,7 @@ loadTemplates((templates, names) => {
 
     function saveTemplate() {
         const selectedTemplate = document.getElementById('templates').value;
-        templates[selectedTemplate] = document.getElementById('template-content').textContent;
+        templates[selectedTemplate] = document.getElementById('template-content').value;
 
         // Save templates to the database
         saveTemplates(templates, names);
@@ -30,15 +50,14 @@ loadTemplates((templates, names) => {
     function addTemplate() {
         const templateName = prompt("Enter the name of the new template:");
         if (templateName) {
-            const templateId = `template${Object.keys(templates).length + 1}`;
-            templates[templateId] = "";
-            names[templateId] = templateName;
+            const templateKey = `template${Object.keys(templates).length + 1}`;
+            templates[templateKey] = '';
+            names[templateKey] = templateName;
+
             const option = document.createElement('option');
-            option.value = templateId;
+            option.value = templateKey;
             option.textContent = templateName;
             document.getElementById('templates').appendChild(option);
-            document.getElementById('templates').value = templateId;
-            updateTemplateContent.call({ value: templateId });
 
             // Save templates to the database
             saveTemplates(templates, names);
@@ -47,25 +66,23 @@ loadTemplates((templates, names) => {
 
     function renameTemplate() {
         const selectedTemplate = document.getElementById('templates').value;
-        const templateName = prompt("Enter the new name of the template:", names[selectedTemplate]);
-        if (templateName) {
-            const option = document.querySelector(`#templates option[value=${selectedTemplate}]`);
-            option.textContent = templateName;
-            names[selectedTemplate] = templateName;
+        const newName = prompt("Enter the new name for the template:", names[selectedTemplate]);
+        if (newName) {
+            names[selectedTemplate] = newName;
+            const option = document.querySelector(`#templates option[value="${selectedTemplate}"]`);
+            option.textContent = newName;
 
             // Save templates to the database
             saveTemplates(templates, names);
         }
     }
 
-    // Populate the templates dropdown
-    const templatesSelect = document.getElementById('templates');
-    templatesSelect.innerHTML = '';
-    for (const templateId in names) {
+    // Populate the template dropdown
+    for (const key in names) {
         const option = document.createElement('option');
-        option.value = templateId;
-        option.textContent = names[templateId];
-        templatesSelect.appendChild(option);
+        option.value = key;
+        option.textContent = names[key];
+        document.getElementById('templates').appendChild(option);
     }
 
     document.getElementById('templates').addEventListener('change', updateTemplateContent);
@@ -73,7 +90,10 @@ loadTemplates((templates, names) => {
     document.getElementById('add-template').addEventListener('click', addTemplate);
     document.getElementById('rename-template').addEventListener('click', renameTemplate);
 
-    updateTemplateContent.call({ value: 'template1' });
+       // Initialize the textarea with the first template
+       if (Object.keys(templates).length > 0) {
+        document.getElementById('templates').selectedIndex = 0;
+        document.getElementById('template-content').value = templates[Object.keys(templates)[0]];
+    }
 });
 
-// ...
